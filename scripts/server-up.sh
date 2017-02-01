902a26fcb690d815d409169eb265a33ea65c059a
Zafar Iqbal <ultrasine@gmail.com>
2017-02-01 10:37:20 +0200

    gitup

---
+++ scripts/server-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+APP=${PWD##*/}     
+
+############################################################
+
+echo hello
+
+#url from config
+
